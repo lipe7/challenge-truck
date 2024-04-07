60d15d31ebedca8ad10e29db8dcfee6f8eaf8c76
felipe <felipec165@gmail.com>
2024-04-07 05:20:15 -0300

    feat: add header x-api-key in tests

--- api/tests/TestCase.php
+++ api/tests/TestCase.php
@@ -2,9 +2,19 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication, DatabaseTransactions;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->withHeaders([
+            'X-API-KEY' => env('API_KEY', '51be5348b9602834fbb67fb562bbd30c42b4c013')
+        ]);
+    }
 }
